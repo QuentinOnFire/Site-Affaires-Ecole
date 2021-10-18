aa10844c82de4ad5d1594df936e90b629208d1e0
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-10-18 19:38:23 +0200

    Update and rename download.js to open_cantine.js

--- js/open_cantine.js
+++ js/open_cantine.js
@@ -5,6 +5,6 @@ var jour = ladate.getDate();
 if (mois === 10 && jour === 16 || jour === 17 || jour === 18 || jour === 19 || jour === 20 || jour === 21 || jour === 22){
     menu = "menu_18_24.png";
 };
-document.write(`<a href="cantine/${menu}" download="menu.png" target="_blank">
+document.write(`<a href="cantine/${menu}" target="_blank">
 <button>Menu Cantine</button>
 </a>`);
