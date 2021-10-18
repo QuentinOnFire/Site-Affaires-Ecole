d081831a9bb7c3efa0cafdb37460d7e53d382110
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-10-18 19:31:00 +0200

    Add files via upload

---
+++ js/blague.js
@@ -0,0 +1,287 @@
+var date = new Date();
+var jour = date.getDate();
+var mois = date.getMonth() + 1;
+var blague = "";
+var intro = "La blague du jour :<br>"
+if (jour === 13 && mois === 10){
+    blague = `
+    Deux amis se rencontrent. L'un, joueur invétéré dit à l'autre :
+    <br>- Il faut que je t'annonce une grande nouvelle : j'arrête de jouer ! Plus de casino, plus de tiercé, plus de poker...
+    <br>- Bravo, lui dit l'autre. Mais pardonne-moi, te connaissant, j'ai du mal à y croire. Je suis même sûr que tu ne tiendras pas !
+    <br>- Ah bon ? Tu paries combien ?`;
+};
+if (jour === 14 && mois === 10){
+    blague = `
+    Deux frères se disputent, l'un dit :
+    <br>- Tête de cochon !
+    <br>- Ho ! quel âne !
+    <br>- Espèce de dinde !
+    <br>Leur mère arrive et dit :
+    <br>- Ho... la ferme !`;
+};
+if (jour === 15 && mois === 10){
+    blague = `
+    <br>Un cow-boy à la carrure impressionnante est en train de boire seul au bar d’un saloon
+    <br>Il finit son verre, paie et sort.
+    <br>Trente secondes après il réapparaît.
+    <br>- Qui a volé mon cheval ?
+    <br>Silence de mort.
+    <br>- Très bien. Je vais attendre cinq minutes au bar, et si dans cinq minutes mon cheval n’est pas là, je fais comme à Carson-City.
+    <br>Il s’installe sur un tabouret. Les cinq minutes écoulées, il se lève, marche lentement vers la porte… et son cheval est là !
+    <br>Tout le monde respire. Alors un vieux cow-boy qui l’a suivi demande timidement :
+    <br>- Qu’est-ce que vous avez fait à Carson-City ?
+    <br>- Je suis rentré chez moi à pied…`;
+};
+if (jour === 16 && mois === 10){
+    blague = `
+    <br>Un milliardaire demande à son épouse :
+    <br>- Chérie, qu’est-ce qui te ferait plaisir pour ton anniversaire ?
+    <br>- Qu’on divorce !
+    <br>- Écoute chérie, pour être très franc, je ne pensais pas mettre aussi cher…`;
+};
+if (jour === 17 && mois === 10){
+    blague = `
+    <br>Un homme rencontre un copain qu’il n’a pas revu depuis plusieurs mois et qui était au chômage.
+    <br>- Alors ? As-tu trouvé du travail ?
+    <br>- Ne m’en parle pas, répond l’autre. J’avais réussi à décrocher un emploi à Limoges, au bout d’un mois ils m’ont limogé.
+    <br>Ensuite j’ai trouvé une place à Vire, quinze jours plus tard je me suis fait virer.
+    <br>Après ça, j’ai été embauché à Lourdes, dix jours après on m’a lourdé.
+    <br>Eh hier, on m’a proposé un boulot à Castres. Alors là, j’ai tout de suite dit non !`;
+};
+if (jour === 18 && mois === 10){
+    blague = `
+    <br>Son père envoie le petit Nicolas au lit car il a été insupportable.
+    <br>Mais au bout de cinq minutes, il entend une petite voix :
+    <br>- Papa, apporte-moi un verre d’eau.
+    <br>- Non, tu n’avais qu’à boire avant…
+    <br>Cinq minutes plus tard :
+    <br>- Papa, apporte-moi un verre d’eau.
+    <br>- Si tu n’arrêtes pas, je monte te donner une fessée.
+    <br>- Papa, quand tu monteras me donner une fessée, est-ce que tu peux m’apporter un verre d’eau ?`;
+};
+if (jour === 19 && mois === 10){
+    blague = `
+    <br>Fin août 1939, un homme apporte ses chaussures à ressemeler chez le cordonnier.
+    <br>Trois jours plus tard, la guerre est déclarée. Mobilisé, il part au front, et il est fait prisonnier. Libéré par les Russes en 45, il se bat avec un officier, ce qui lui vaut dix ans de Goulag. Échangé par les Américains, il se retrouve au États-Unis et s’y installe.
+    <br>Et voilà que quarante ans après, il revient en France en touriste. Bien entendu, cet homme va revoir sa rue. Elle a beaucoup changé, sauf la cordonnerie qui est toujours là. Alors, par curiosité, il entre et demande au vieil homme derrière sa machine :
+    <br>- Il y a longtemps que vous êtes installé ici ?
+    <br>- Oh, mon pauvre monsieur, depuis l’avant-guerre !
+    <br>- Alors c’est à vous que j’avais confié mes chaussures à réparer ! C’était fin août 1939. Trois jours plus tard j’étais mobilisé…
+    <br>Il lui raconte toute son histoire. Et il ajoute :
+    <br>- Au fait, vous les avez peut-être encore mes chaussures ? Ce serait drôle…
+    <br>- Attendez je vais voir… fait le vieux.
+    <br>Il soulève sa trappe et descend au sous-sol. D’en bas il crie :
+    <br>- Elles sont comment, vos chaussures ?
+    <br>- Jaunes !
+    <br>- Avec des bouts carrés et des lacets marron ?
+    <br>- Oui !
+    <br>- Elles seront prêtes jeudi…`;
+};
+if (jour === 20 && mois === 10){
+    blague = `
+    <br>À Liège, un petit garçon rentre de l’école en pleurant :
+    <br>- Qu’est-ce qui t’arrive ? lui demande son père.
+    <br>- J’ai eu un zéro en géographie, Papa…
+    <br>- Et pourquoi ?
+    <br>- Je ne savais pas où est située la Roumanie.
+    <br>- Quoi ?! Bourrique, triple andouille, passe-moi la carte de Belgique !
+    <br>Et le père cherche, cherche ….
+    <br>- Di djû, elle n’est pas assez précise, passe-moi la carte de la province de Liège.
+    <br>Et le père cherche de plus belle…
+    <br>- Mais nom di djû… Ça ne peut pas être si loin la Roumanie , j’ai un copain Roumain qui vient travailler tous les jours à vélo…`;
+};
+if (jour === 21 && mois === 10){
+    blague = `
+    <br>Un couple part en voyage aux Etats-Unis.
+    <br>Au moment de reprendre l’avion vers l’Europe, la femme demande à son mari:
+    <br>- Marcel ? Crois-tu qu’à la douane, ils vont nous laisser passer avec la chauve-souris et le putois que tu comptes offrir à tes amis ?
+    <br>- Et bien oui ! Il n’y a pas de problème ! Pour la chauve-souris, je vais la plaquer sur mon Tee-shirt. Les Américains penseront qu’il s’agit d’un Tee-shirt de BatMan.
+    <br>- Oui d’accord, mais pour le putois ?
+    <br>- Pour le putois, on va le mettre dans ta culotte.
+    <br>- Dans ma culotte ? Mais l’odeur ?
+    <br>- Tant pis chérie. S’il meurt, il meurt !`;
+};
+if (jour === 22 && mois === 10){
+    blague = `
+    <br>Une femme entre dans sa salle de bain et voit son mari tout nu, en train de se peser, tout en rentrant son ventre. Elle rigole et lui dit :
+    <br>- Tu sais, tu as beau rentrer ton ventre, ça ne fera pas descendre ton poids !
+    <br>- Je sais bien, répond-il vexé, mais au moins ça me permet de le lire !`;
+};
+if (jour === 23 && mois === 10){
+    blague = `
+    <br>Un grand antiquaire parcourt la campagne pour dénicher au meilleur prix des meubles ou des objets anciens.
+    <br>Il arrive dans une ferme et voit un petit chat qui boit son lait dans une écuelle qu’il identifie immédiatement comme datant de l’époque romaine. Une pure merveille !
+    <br>Cachant du mieux possible son enthousiasme, il dit au paysan :
+    <br>- Ma femme a toujours eu envie d’avoir un chat. Si vous voulez vous débarrasser de celui-là, je vous l’achète…
+    <br>- Pourquoi pas ? répond le fermier. Si vous me donnez deux cents euros, il est à vous.
+    <br>- Marché conclu ! dit l’antiquaire.
+    <br>Il sort deux billets de cent euros, les donne et ajoute nonchalamment :
+    <br>- Pour qu’il ne soit pas trop dépaysé, je vais emporter son bol…
+    <br>- Ah non ! fait le fermier, le bol vous le laissez ici ! Depuis le début de l’année, il m’a déjà fait vendre quinze chats…`;
+};
+if (jour === 24 && mois === 10){
+    blague = `
+    <br>Un singe rentre dans un bar et demande au barman:
+    <br>– Vous avez des bananes ?
+    <br>– Non, on n’a pas de bananes.
+    <br>– Vous avez des bananes ?
+    <br>– Non, on n'en a pas.
+    <br>– Vous avez des bananes ?
+    <br>– Non, t’es sourd ou quoi !! Si tu me demandes encore si j’ai des bananes je te cloue la langue au comptoir !!!!
+    <br>– Vous avez des clous ?
+    <br>– Non.
+    <br>– Vous avez des bananes ?`;
+};
+if (jour === 25 && mois === 10){
+    blague = `
+    <br>Georges, 88 ans, rend visite à son médecin pour un examen de routine.
+    <br>- Comment ça va, Georges ?
+    <br>- Très bien Docteur, Dieu veille sur moi.
+    <br>- Que voulez-vous dire ?
+    <br>- Ben, par exemple, la nuit dernière je me suis réveillé pour aller uriner. En ouvrant la porte des toilettes, hop, la lumière s’allume toute seule ; Dieu veille sur moi. Quand j’ai eu fini, je referme la porte et hop, la lumière s’éteint encore toute seule ; Dieu veille sur moi.
+    <br>Le docteur, intrigué par ces révélations, décide de téléphoner à l’épouse de Georges…
+    <br>- Georges m’a rendu visite aujourd’hui et m’a dit que Dieu veille sur lui et qu’il lui a allumé et éteint la lumière pour uriner cette nuit, est-ce exact ?
+    <br>- Sacré nomdidju ! Ne me dites pas qu’il a encore pissé dans le frigo !`;
+};
+if (jour === 26 && mois === 10){
+    blague = `
+    <br>Un couple se déchire au cours d’un jugement pour divorce.
+    <br>La femme et le mari se disputent la garde de leur enfant unique.
+    <br>La maman, très émue, prend la parole :
+    <br>- Votre Honneur… Ce petit garçon a été conçu en moi… Il est sorti de mon ventre… Il est de ma chair… Je mérite de le garder !
+    <br>Le juge, ému par cette défense et presque convaincu, laisse la parole au mari.
+    <br>Le mari se lève, regarde le juge dans les yeux et dit :
+    <br>- Votre Honneur, je ne ferai pas de longues phrases… je n’aurai qu’une question. Dans le couloir du tribunal, quand vous introduisez une pièce dans le distributeur de boissons, la canette de soda qui en sort est à vous ou à la machine ?`;
+};
+if (jour === 27 && mois === 10){
+    blague = `
+    <br>Trois petits vieux qui étaient de bons amis se retrouvent après de longues années :
+    <br>- Que faites-vous depuis que vous êtes en retraite ?
+    <br>Le premier dit :
+    <br>- Moi, je fais de la photo.
+    <br>Le deuxième :
+    <br>- Moi, je jardine.
+    <br>Et le troisième annonce :
+    <br>- Moi, je fais de la recherche… 
+    <br>- Ah bon ! Et dans quoi ?
+    <br>- Tous les jours, je cherche mes lunettes, ma canne, mon dentier, mes clés…`;
+};
+if (jour === 28 && mois === 10){
+    blague = `
+    <br>Une vedette du show-biz fait repeindre son appartement.
+    <br>Pour faire plaisir au peintre, il lui donne une invitation pour son nouveau spectacle à l’Olympia.
+    <br>Quand il reçoit la facture des travaux, à la rubrique Heures supplémentaires il est écrit : "Deux heures à regarder le client chanter et danser : cent euros…"`;
+};
+if (jour === 29 && mois === 10){
+    blague = `
+    <br>Une petite fille est en train de feuilleter l’album de famille.
+    <br>Tout à coup, elle demande à sa mère :
+    <br>- Maman ! Qui c’est le beau brun en maillot de bain, avec des pectoraux terribles, qui est à côté de toi sur la plage ?
+    <br>- C’est ton papa quand je l’ai connu, répond la mère d’un ton mélancolique.
+    <br>- Ah bon ! C’est mon papa ? Ben alors, qui c’est le gros type chauve qui vit à la maison ?`;
+};
+if (jour === 30 && mois === 10){
+    blague = `
+    <br>Un émir souffrant d’une grave maladie cardiaque est venu se faire opérer à Paris par un spécialiste.
+    <br>L’opération est réussie et avant de repartir dans son pays, l’émir dit au chirurgien :
+    <br>- Docteur, sis je suis en vie, c’est grâce à vous. J’aimerais vous faire un cadeau pour vous remercier.
+    <br>- Mais je n’ai fait que mon travail…
+    <br>- J’insiste, docteur. Qu’est-ce qui vous ferait plaisir ?
+    <br>- Puisque vous y tenez vraiment, sachez que j’adore jouer au golf. Si vous m’offrez quelques clubs, je serais ravi.
+    <br>Un mois plus tard, le chirurgien reçoit un télégramme de l’émir :
+    <br>Suis heureux de vous offrir douze clubs de golf. Stop. Désolé, mais deux d’entre eux n’ont pas de piscine...`;
+};
+if (jour === 31 && mois === 10){
+    blague = `
+    <br>- Dis, papa, pourquoi tu es toujours tout rouge ?
+    <br>- Euh… Ben… L’hiver, quand je rentre du travail en mobylette, le froid me rend tout rouge.
+    <br>- Ah bon ! Mais pourquoi l’été aussi tu es tout rouge ?
+    <br>- Eh ben, c’est à cause du casque. Quand je reviens en mobylette, ça me donne chaud.
+    <br>Alors la mère se lève brusquement et saisit la bouteille de pastis. Son mari, étonné, lui demande :
+    <br>- Mais qu’est-ce que tu fais, chérie ?
+    <br>- Comme tu le vois, je range la mobylette.`;
+};
+if (jour === 1 && mois === 11){
+    blague = `
+    <br>Dans un ministère, les souris ont envahi les sous-sols où sont entreposées des tonnes d’archives.
+    <br>Un des chefs de service connaît un ami qui possède un chat réputé bon chasseur de souris et il lui demande de lui prêter.
+    <br>Au bout de quinze jours il n’y a plus une seule souris et du coup le ministère décide d’acheter le chat pour le garder en permanence.
+    <br>Deux mois après le chef de service revoit son ami.
+    <br>- Je ne comprends pas. Ton chat était formidable au début, et depuis quelque temps les souris sont revenues et il ne fait rien !
+    <br>- C’est normal, dit l’autre. Vous l’avez titularisé…`;
+};
+if (jour === 2 && mois === 11){
+    blague = `
+    <br>Un beau matin une petite vieille dame répond lorsqu’on frappe à sa porte.
+    <br>Elle y découvre un jeune homme, bien habillé avec un aspirateur à la main.
+    <br>"Bonjour Madame, dit le jeune homme. Si vous avez une minute je vais vous montrer ce nouvel aspirateur, haute technologie, qui aspire sans comparaison !"
+    <br>"Allez-vous en, lui dit-elle, je suis fauchée." Et elle tente de refermer la porte
+    <br>Aussi rapide qu’un éclair, le jeune homme met son pied pour coincer la porte ouverte.
+    <br>Il rouvre la porte et s’exclame: "pas si rapidement, Madame, pas avant que je vous ai fait ma démonstration."
+    <br>Sur ce, il vide un sac plein de crottes de lapin sur le tapis
+    <br>"Maintenant, Madame, si mon aspirateur ne nettoie pas la totalité de ce tas de merde, je m’engage à manger ce qui en restera !"
+    <br>La vieille dame recule et lui dit:  "Je vais vous chercher une cuillère, Monsieur, ils ont coupé mon électricité ce matin parce que je n’ai pas payé ma facture".`;
+};
+if (jour === 3 && mois === 11){
+    blague = `
+    <br>Le jour de l’ouverture de son magasin, un commerçant reçoit un bouquet de fleurs. Sur la carte jointe, il découvre les mots suivants : « Nos sincères condoléances ».
+    <br>Le fleuriste lui téléphone pour lui présenter ses excuses, ce bouquet ne lui était pas destiné.
+    <br>– Oh ce n’est pas grave, cela peut arriver !
+    <br>– Ce qui me gêne, c’est que le bouquet qui vous était destiné, ainsi que la carte sont partis pour le cimetière.
+    <br>– Ah ! Qu’y avait-il d’inscrit sur cette carte ?
+    <br>– « Félicitations pour votre nouvel appartement ».`;
+};
+if (jour === 4 && mois === 11){
+    blague = `
+    <br>Un incendie ravage un immeuble de Bruxelles.
+    <br>Bloquée au troisième étage, une femme tient un bébé dans ses bras.
+    <br>Lancez le petit ! crient les pompiers qui ont tendu une toile.
+    <br>Non, je ne saurais pas ! hurle la femme. C’est mon enfant, j’ai peur que vous le manquiez ! Je veux le goal de l’équipe de football de Belgique !
+    <br>Mais, madame, s’écrie le capitaine, le temps qu’il arrive, il sera trop tard…
+    <br>Je sais où il habite, dit un pompier. C’est tout à côté…
+    <br>On court chez lui. Miracle, il est là. En un clin d’œil il enfile son maillot, son short, ses chaussures, met sa casquette et il arrive trois minutes plus tard devant l’immeuble en feu où la malheureuse est toujours coincée.
+    <br>Vous me reconnaissez, madame ? Je suis le goal de l’équipe de Belgique. C’est moi qui ai arrêté le penalty dimanche contre l’Allemagne ! Vous pouvez lancer le petit !
+    <br>Vous allez l’attraper ?
+    <br>Aucun problème, madame, comme à l’entraînement…
+    <br>Alors elle lance le bébé, le goal se détend, le cueille du bout des doigts, le serre dans ses bras, et le pose délicatement au sol. Après quoi, il prend cinq mètres d’élan et dégage.`;
+};
+if (jour === 5 && mois === 11){
+    blague = `
+    <br>Le représentant d’une association charitable frappe à la porte d’une maison :
+    <br>-Bonjour madame, dit-il, nous faisons une collecte pour le centre d’accueil des alcooliques.
+    <br>-Repassez ce soir après dix-neuf heures, je vous donnerai mon mari…`;
+};
+if (jour === 6 && mois === 11){
+    blague = `
+    <br>Une dame a acheté un kilo de poisson au marché.
+    <br>Elle le pose sur la table de la cuisine et redescend parce qu’elle a oublié de prendre des citrons.
+    <br>Quand elle revient cinq minutes plus tard, plus de poisson. Et sous la table, le chat se pourlèche les babines.
+    <br>Alors la dame l’attrape par la peau du dos et lui dit :
+    <br>-Tu as mangé le poisson, hein, sale chat ! Je le sais que c’est toi ! D’ailleurs je vais le prouver…
+    <br>Et elle colle le chat sur la balance. L’aiguille indique un kilo. Alors la dame ébahie murmure :
+    <br>-Ça alors ! Où est passé le chat ?`;
+};
+if (jour === 7 && mois === 11){
+    blague = `
+    <br>Une cliente entre dans une boucherie et demande un beau poulet.
+    <br>Le boucher va dans l’arrière-boutique et ramène le seul poulet qui lui reste.
+    <br>-Voilà, chère madame.
+    <br>-Vous n’en auriez pas un plus gros ?
+    <br>-Je vais voir.
+    <br>Il repart de l’autre côté, prend une pompe à vélo, met l’extrémité dans le croupion du poulet, et donne trois ou quatre coups de pompe pour le gonfler.
+    <br>Puis il le rapporte :
+    <br>-Celui-là vous convient ?
+    <br>-Parfait ! Finalement, je vais prendre les deux…`;
+};
+if (jour === 8 && mois === 11){
+    blague = `
+    <br>Un type veut vendre sa vieille voiture, mais sa voiture a 450 000 kilomètres et personne n’en veut. Il décide alors de demander conseil à un copain, lequel lui dit :
+    <br>- Accepterais-tu de faire quelque chose d’illégal ?
+    <br>- Oui, je veux vendre ma voiture à tout prix.
+    <br>- Bon ! Dans ce cas, tu vas voir mon ami Tony qui est mécanicien, et il va mettre ton compteur à 45 000 kilomètres.
+    <br>Le gars va donc voir Tony, qui lui remet le compteur à 45 000 kilomètres.
+    <br>Quelques jours plus tard il revoit son copain, qui lui demande :
+    <br>- As-tu vendu ta voiture ?
+    <br>- Ça ne va pas, non ! Maintenant qu’elle a 45 000 kilomètres, je la garde !`;
+};
+document.write(intro + blague);
---
+++ js/nom_jour.js
@@ -0,0 +1,3 @@
+var ladate=new Date();
+var tab_jour=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+document.write(tab_jour[ladate.getDay()]);
