04ec8705d452aee2c9580115bd2a36bc97f04a74
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-11-11 13:21:08 +0100

    Update 4_3.js

--- js/4_3.js
+++ js/4_3.js
@@ -29,7 +29,7 @@ console.warn(`Nous sommes a ${numberOfDays} jours du 4 janvier 2021 (debut semai
 function p_i(paire, impaire){
     result= result_base;
 
-    /*if (nb_jour === 6 || nb_jour === 0){
+    if (nb_jour === 6 || nb_jour === 0){
         result += 1;
     }
     result = result / 2;
@@ -39,7 +39,7 @@ function p_i(paire, impaire){
     else{
         console.log("PAIRE");
         return paire;
-    }*/
+    }
     return impaire;
 }
 
