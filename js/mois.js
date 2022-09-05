2c4516f6d7d0a65cfc2169c785513aa0d3ac9599
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2022-09-05 20:25:01 +0200

    Delete js directory

--- js/mois.js
+++
@@ -1,3 +0,0 @@
-var ladate=new Date();
-var tab_mois=new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
-document.write(tab_mois[ladate.getMonth()])
